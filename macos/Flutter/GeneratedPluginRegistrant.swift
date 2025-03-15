7a0eb39c28e731e25e74a18fb47fd51716da39b9
TJMusiitwa <jonamusiitwa@outlook.com>
2025-03-15 19:36:46 +0300

    Update project configuration and dependencies to 3.29

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,7 @@ import Foundation
 
 import path_provider_foundation
 import share_plus
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
